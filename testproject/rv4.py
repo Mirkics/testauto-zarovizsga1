4ce4301835edab639a8f5da47f5f4ff35be8c768
Mirkics <eri_opre@yahoo.co.uk>
2021-08-09 20:41:32 +0200

    code

--- testproject/rv4.py
+++ testproject/rv4.py
@@ -16,8 +16,26 @@ try:
     driver.get('https://black-moss-0a0440e03.azurestaticapps.net/rv4.html')
     time.sleep(2)
 
-    cities_list = driver.find_element_by_xpath('//*[@id="cites"]/text()')
-    print(cities_list)
+# mezők lekérdezése
+
+    cities = driver.find_element_by_id('cites').text
+    print(cities)
+    miss_city = driver.find_element_by_id("missingCity")
+    check_btn = driver.find_element_by_id("submit")
+    result = driver.find_element_by_id('result').text
+    random_cities_list = driver.find_elements_by_xpath('//*[@id="randomCities"]/li')
+    print(random_cities_list)
+
+# kivenni egy listába a város neveket és összehasonlítani a random_cities_list elemeivel
+
+
+# for i in range(len(missing_city)):
+#         if cities[i] == missing_city[i]:
+
+
+#   assert result == "Eltaláltad."
+
+
 
 
 finally:
